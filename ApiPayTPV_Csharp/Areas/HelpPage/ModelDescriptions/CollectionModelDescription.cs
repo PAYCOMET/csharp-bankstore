3c7be771204b3bfd2a013ccd9e308f33c4810261
Diego Marcos Camarasa <dmc@paytpv.com>
2016-06-13 14:00:48 +0200

    Primera subida del API: C# y ASP.NET. Está en BETA, se añadirán nuevas actualizaciones.

---
+++ ApiPayTPV_Csharp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ApiPayTPV_Csharp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
